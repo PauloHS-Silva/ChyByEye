8ac6548e86ad7e6ad64dfc13b632e0e9c20803ae
Paulo Silva <pauloh@mit.edu>
2025-04-18 14:41:38 -0400

    fixed some deployment issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   const location = useLocation();
   return (
     <nav className="navbar">
-      <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
-      <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About</Link>
-      <Link to="/download" className={location.pathname === '/download' ? 'active' : ''}>Download</Link>
-      <Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>Contact</Link>
+      <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+        Home
+      </Link>
+      <Link
+        to="/about"
+        className={location.pathname === "/about" ? "active" : ""}
+      >
+        About
+      </Link>
+      <Link
+        to="/download"
+        className={location.pathname === "/download" ? "active" : ""}
+      >
+        Download
+      </Link>
+      <Link
+        to="/contact"
+        className={location.pathname === "/contact" ? "active" : ""}
+      >
+        Contact
+      </Link>
     </nav>
   );
 };
